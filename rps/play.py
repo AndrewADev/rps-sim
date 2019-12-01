46a714ffeb2f4b7d848fb63200a76cd0d9c05d96
Andrew A <AndrewADev@gmail.com>
2019-12-01 14:54:55 +0100

    Rudimentary match logic
    Compare the plays against each other. Complete with some initial tests.

--- rps/play.py
+++ rps/play.py
@@ -1,9 +1,24 @@
 from enum import IntEnum
 
+class PlayResult(IntEnum):
+  LOSS = -1
+  DRAW = 0
+  WIN = 1
+
 class Play(IntEnum):
   ROCK = 1
   PAPER = 2
   SCISSORS = 3
+
+  def match(self, other):
+    if self.value == other.value or type(other) is not Play:
+      return PlayResult.DRAW
+    if self.value == self.ROCK:
+      return PlayResult.WIN if other.value == self.SCISSORS else PlayResult.LOSS
+    elif self.value == self.SCISSORS:
+      return PlayResult.WIN if other.value == self.PAPER else PlayResult.LOSS
+    elif self.value == self.PAPER:
+      return PlayResult.WIN if other.value == self.ROCK else PlayResult.LOSS
 
   @staticmethod
   def min():
