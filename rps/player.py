ad300ef3cc3b2eed0576356b76b863cd96d449fb
Andrew Armbruster <AndrewADev@gmail.com>
2023-08-13 21:41:57 +0200

    feat: allow specifying names of players
    We allow specifying both, though for now only Player 1's name is shown.

--- rps/player.py
+++ rps/player.py
@@ -5,14 +5,17 @@ from .strategy import PlaySelectionStrategy
 
 
 class Player(object):
+    name: str
+
     @abc.abstractmethod
     def play(self) -> Play:
         """Required method"""
 
 
 class BotPlayer(Player):
-    def __init__(self, play_strategy: PlaySelectionStrategy):
+    def __init__(self, play_strategy: PlaySelectionStrategy, name: str = "Bot Player"):
         self.__strategy = play_strategy
+        self.name = name
 
     def play(self)  -> Play:
         return self.__strategy.play()
